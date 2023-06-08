d2f1bf97338d22bcfeaf44cb8d5ae25984063f1e
Pavlo Vasylenko <userow@gmail.com (skype: userow, telegram: @ag_leo)>
2023-06-08 11:12:32 +0300

    Initial Commit

---
+++ CameraFilter-RxSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CameraFilter-RxSwift
+//
+//  Created by Pavlo Vasylenko on 08.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
